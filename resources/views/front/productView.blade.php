ac3a3398d48077ea51768d2d9dedcb2ea89a99ef
Tasnim-moon <nowrinmoon44@gmail.com>
2022-08-26 01:39:37 +0600

    first commit

---
+++ resources/views/front/productView.blade.php
@@ -0,0 +1,131 @@
+@extends('front.layout.layout')
+
+@section('content')
+
+	<div class="span9">
+    <ul class="breadcrumb">
+    <li><a href="index.html">Home</a> <span class="divider">/</span></li>
+    <li><a href="products.html">Products</a> <span class="divider">/</span></li>
+    <li class="active">product Details</li>
+    </ul>	
+	<div class="row">	
+			<div id="gallery" class="span3">
+            <a href="themes/images/products/large/f1.jpg" title="Home Decor">
+				<img src="{{asset('uploads/'.$product->image)}}" style="width:100%" alt="Home Decor"/>
+            </a>
+			
+			  
+			 <div class="btn-toolbar">
+			  <div class="btn-group">
+				<span class="btn"><i class="icon-envelope"></i></span>
+				<span class="btn" ><i class="icon-print"></i></span>
+				<span class="btn" ><i class="icon-zoom-in"></i></span>
+				<span class="btn" ><i class="icon-star"></i></span>
+				<span class="btn" ><i class=" icon-thumbs-up"></i></span>
+				<span class="btn" ><i class="icon-thumbs-down"></i></span>
+			  </div>
+			</div>
+			</div>
+			<div class="span6">
+				<h3>{{$product->ProductDetail->title}}</h3>
+				<hr class="soft"/>
+				  <div class="control-group">
+					<label class="control-label"><span>${{$product->price}}</span></label>
+					<div class="controls">
+						<form method="post" action="{{route('cart.store')}}">
+							@csrf
+							<input type="number" class="span1" name="qty" placeholder="Qty."/>
+							<input type="hidden" value="{{$product->id}}" name="product_id" readonly>
+							@if(Auth::user())
+						  	<button type="submit" class="btn btn-large btn-primary pull-right"> Add to cart <i class=" icon-shopping-cart"></i></button>
+							@else
+								<button class="btn btn-large btn-primary pull-right"><a style="color: #fff;text-decoration: none;" href="{{route('user_login')}}"> Add to cart <i class=" icon-shopping-cart"></i></a></button>
+							@endif
+						</form>
+					</div>
+				  </div>
+				
+				<hr class="soft"/>
+				<h4>{{$product->ProductDetail->total_items}} items in stock</h4>
+				<form class="form-horizontal qtyFrm pull-right">
+				  <div class="control-group">
+					<label class="control-label"><span>Color</span></label>
+					<div class="controls">
+					  <select class="span2">
+						  <option>Black</option>
+						  <option>Red</option>
+						  <option>Blue</option>
+						  <option>Brown</option>
+						</select>
+					</div>
+				  </div>
+				</form>
+				<hr class="soft clr"/>
+				<p>
+				{{$product->ProductDetail->description}}
+				
+				</p>
+				<a class="btn btn-small pull-right" href="#detail">More Details</a>
+				<br class="clr"/>
+			<a href="#" name="detail"></a>
+			<hr class="soft"/>
+			</div>
+			
+			<div class="span9">
+            <ul id="productDetail" class="nav nav-tabs">
+              <li class="active"><a href="#home" data-toggle="tab">Product Details</a></li>
+              <li><a href="#profile" data-toggle="tab">Related Products</a></li>
+            </ul>
+            <div id="myTabContent" class="tab-content">
+              <div class="tab-pane fade active in" id="home">
+			  <h4>Product Information</h4>
+                <table class="table table-bordered">
+				<tbody>
+				
+				</tbody>
+				</table>
+				
+				<h5>Features</h5>
+				<p>
+				Beautifully Designed
+				</p>
+
+				<h4>Editorial Reviews</h4>
+				<h5>Manufacturer's Description </h5>
+				<p>
+				Hand Made
+				</p>
+
+				
+				
+              </div>
+		<div class="tab-pane fade" id="profile">
+		<br class="clr"/>
+		<hr class="soft"/>
+		<div class="tab-content">
+			<div class="tab-pane active" id="blockView">
+				<ul class="thumbnails">
+					@foreach($related_products as $related_product)
+					<li class="span3">
+					  <div class="thumbnail">
+						<a href="product_details.html"><img src="{{asset('uploads/'.$related_product->image)}}" alt=""/></a>
+						<div class="caption">
+						  <h5>{{$related_product->name}}</h5>
+						  <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&euro;{{$related_product->price}}</a></h4>
+						</div>
+					  </div>
+					</li>
+					@endforeach
+				  </ul>
+			<hr class="soft"/>
+			</div>
+		</div>
+				<br class="clr">
+					 </div>
+		</div>
+          </div>
+
+	</div>
+</div>
+
+@endsection
---
+++ resources/views/front/specialOffer.blade.php
@@ -0,0 +1,321 @@
+@extends('front.layout.layout')
+
+@section('content')
+
+	<div class="span9">
+    <ul class="breadcrumb">
+		<li><a href="index.html">Home</a> <span class="divider">/</span></li>
+		<li class="active">Special offers</li>
+    </ul>
+	<h4> 20% Discount Special offer<small class="pull-right"> 40 products are available </small></h4>	
+	<hr class="soft"/>
+	<form class="form-horizontal span6">
+		<div class="control-group">
+		  <label class="control-label alignL">Sort By </label>
+			<select>
+              <option>Product name A - Z</option>
+              <option>Product name Z - A</option>
+              <option>Product Stoke</option>
+              <option>Price Lowest first</option>
+            </select>
+		</div>
+	  </form>
+	<div id="myTab" class="pull-right">
+	 <a href="#listView" data-toggle="tab"><span class="btn btn-large"><i class="icon-list"></i></span></a>
+	 <a href="#blockView" data-toggle="tab"><span class="btn btn-large btn-primary"><i class="icon-th-large"></i></span></a>
+	</div>
+<br class="clr"/>
+<div class="tab-content">
+	<div class="tab-pane" id="listView">
+		<div class="row">	  
+			<div class="span2">
+			<img src="themes/images/products/box1.jpg" alt=""/>
+			</div>
+			<div class="span4">
+				<h3>New | Available</h3>				
+				<hr class="soft"/>
+				<h5>Product Name </h5>
+				<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s ...
+				</p>
+				<a class="btn btn-small pull-right" href="product_details.html">View Details</a>
+				<br class="clr"/>
+			</div>
+			<div class="span3 alignR">
+			<form class="form-horizontal qtyFrm">
+			<h3> $110.00</h3>
+			<label class="checkbox">
+				<input type="checkbox">  Adds product to compare
+			</label><br/>
+			  <a href="product_details.html" class="btn btn-large btn-primary"> Add to <i class=" icon-shopping-cart"></i></a>
+			  <a href="product_details.html" class="btn btn-large"><i class="icon-zoom-in"></i></a>
+				</form>
+			</div>
+	</div>
+	<hr class="soft"/>
+	<div class="row">	  
+			<div class="span2">
+				<img src="themes/images/products/box2.jpg" alt=""/>
+			</div>
+			<div class="span4">
+				<h3>New | Available</h3>				
+				<hr class="soft"/>
+				<h5>Product Name </h5>
+				<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s ...
+				</p>
+				<a class="btn btn-small pull-right" href="product_details.html">View Details</a>
+				<br class="clr"/>
+			</div>
+			<div class="span3 alignR">
+			<form class="form-horizontal qtyFrm">
+				<h3> $110.00</h3>
+				<label class="checkbox">
+				<input type="checkbox">  Adds product to compare
+				</label><br/>
+				<a href="product_details.html" class="btn btn-large btn-primary"> Add to <i class=" icon-shopping-cart"></i></a>
+				<a href="product_details.html" class="btn btn-large"><i class="icon-zoom-in"></i></a>
+			</form>
+			</div>
+	</div>
+	<hr class="soft"/>
+	<div class="row">	  
+			<div class="span2">
+				<img src="themes/images/products/b3.jpg" alt=""/>
+			</div>
+			<div class="span4">
+				<h3>New | Available</h3>				
+				<hr class="soft"/>
+				<h5>Product Name </h5>
+				<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s ...
+				</p>
+				<a class="btn btn-small pull-right" href="product_details.html">View Details</a>
+				<br class="clr"/>
+			</div>
+			<div class="span3 alignR">
+			<form class="form-horizontal qtyFrm">
+			<h3> $110.00</h3>
+			<label class="checkbox">
+			<input type="checkbox">  Adds product to compare
+			</label><br/>
+			  <a href="product_details.html" class="btn btn-large btn-primary"> Add to <i class=" icon-shopping-cart"></i></a>
+			  <a href="product_details.html" class="btn btn-large"><i class="icon-zoom-in"></i></a>
+			</form>
+			</div>
+	</div>
+	<hr class="soft"/>
+	<div class="row">	  
+			<div class="span2">
+				<img src="themes/images/products/b4.jpg" alt=""/>
+			</div>
+			<div class="span4">
+				<h3>New | Available</h3>				
+				<hr class="soft"/>
+				<h5>Product Name </h5>
+				<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s ...
+				</p>
+				<a class="btn btn-small pull-right" href="product_details.html">View Details</a>
+				<br class="clr"/>
+			</div>
+			<div class="span3 alignR">
+				<form class="form-horizontal qtyFrm">
+				<h3> $110.00</h3>
+				<label class="checkbox">
+				<input type="checkbox">  Adds product to compare
+				</label><br/>
+				<a href="product_details.html" class="btn btn-large btn-primary"> Add to <i class=" icon-shopping-cart"></i></a>
+				<a href="product_details.html" class="btn btn-large"><i class="icon-zoom-in"></i></a>
+				</form>
+			</div>
+	</div>
+	
+	<hr class="soft"/>
+	<div class="row">	  
+			<div class="span2">
+				<img src="themes/images/products/6.jpg" alt=""/>
+			</div>
+			<div class="span4">
+				<h3>New | Available</h3>				
+				<hr class="soft"/>
+				<h5>Product Name </h5>
+				<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s ...
+				</p>
+				<a class="btn btn-small pull-right" href="product_details.html">View Details</a>
+				<br class="clr"/>
+			</div>
+			<div class="span3 alignR">
+				<form class="form-horizontal qtyFrm">
+				<h3> $222.00</h3>
+				<label class="checkbox">
+				<input type="checkbox">  Adds product to compare
+				</label><br/>
+				<a href="product_details.html" class="btn btn-large btn-primary"> Add to <i class=" icon-shopping-cart"></i></a>
+				<a href="product_details.html" class="btn btn-large"><i class="icon-zoom-in"></i></a>
+				</form>
+			</div>
+	</div>
+	<hr class="soft"/>
+		<div class="row">	  
+			<div class="span2">
+				<img src="themes/images/products/7.jpg" alt=""/>
+			</div>
+			<div class="span4">
+				<h3>New | Available</h3>				
+				<hr class="soft"/>
+				<h5>Product Name </h5>
+				<p>
+				
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s ...
+				</p>
+				<a class="btn btn-small pull-right" href="product_details.html">View Details</a>
+				<br class="clr"/>
+			</div>
+			<div class="span3 alignR">
+				<form class="form-horizontal qtyFrm">
+				<h3> $222.00</h3>
+				<label class="checkbox">
+				<input type="checkbox">  Adds product to compare
+				</label><br/>
+				<a href="product_details.html" class="btn btn-large btn-primary"> Add to <i class=" icon-shopping-cart"></i></a>
+				<a href="product_details.html" class="btn btn-large"><i class="icon-zoom-in"></i></a>
+				</form>
+			</div>
+		</div>
+	<hr class="soft"/>
+	</div>
+
+	<div class="tab-pane  active" id="blockView">
+		<ul class="thumbnails">
+			<li class="span3">
+			  <div class="thumbnail">
+				<a href="product_details.html"><img src="themes/images/products/box1.jpg" alt=""/></a>
+				<div class="caption">
+				  <h5>Jwellery Box</h5>
+				  <p> 
+					Ornaments holder 
+				  </p>
+				  <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&euro;110.00</a></h4>
+				</div>
+			  </div>
+			</li>
+			<li class="span3">
+			  <div class="thumbnail">
+				<a href="product_details.html"><img src="themes/images/products/box2.jpg" alt=""/></a>
+				<div class="caption">
+				  <h5>Jwellery Box</h5>
+				  <p> 
+				  	Ornaments holder 
+				  </p>
+				  <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&euro;110.00</a></h4>
+				</div>
+			  </div>
+			</li>
+			<li class="span3">
+			  <div class="thumbnail">
+				<a href="product_details.html"><img src="themes/images/products/box3.jpg" alt=""/></a>
+				<div class="caption">
+				  <h5>Jwellery Box</h5>
+				  <p> 
+					Ornaments holder
+				  </p>
+				   <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&euro;110.00</a></h4>
+				</div>
+			  </div>
+			</li>
+			<li class="span3">
+			  <div class="thumbnail">
+				<a href="product_details.html"><img src="themes/images/products/box4.jpg" alt=""/></a>
+				<div class="caption">
+				  <h5>Jwellery Box</h5>
+				  <p> 
+				    Ornaments holder 
+				  </p>
+				   <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&euro;110.00</a></h4>
+				</div>
+			  </div>
+			</li>
+			<li class="span3">
+			  <div class="thumbnail">
+				<a href="product_details.html"><img src="themes/images/products/home1.jpg" alt=""/></a>
+				<div class="caption">
+				  <h5>Home Decor</h5>
+				  <p> 
+					beautiful wall hanging 
+				  </p>
+				   <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&euro;222.00</a></h4>
+				</div>
+			  </div>
+			</li>
+			<li class="span3">
+			  <div class="thumbnail">
+				<a href="product_details.html"><img src="themes/images/products/decor1.jpg" alt=""/></a>
+				<div class="caption">
+				  <h5>Home Decor</h5>
+				  <p> 
+					white colour wall hanging
+				  </p>
+				   <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&euro;222.00</a></h4>
+				</div>
+			  </div>
+			</li>
+			<li class="span3">
+			  <div class="thumbnail">
+				<a href="product_details.html"><img src="themes/images/products/Home2.jpg" alt=""/></a>
+				<div class="caption">
+				  <h5>Home Decor</h5>
+				  <p> 
+					soothing showpiece
+				  </p>
+				   <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&euro;222.00</a></h4>
+				</div>
+			  </div>
+			</li>
+			<li class="span3">
+			  <div class="thumbnail">
+				<a href="product_details.html"><img src="themes/images/products/decor2.jpg" alt=""/></a>
+				<div class="caption">
+				  <h5>Home Decor</h5>
+				  <p> 
+					designable tray 
+				  </p>
+				   <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&euro;222.00</a></h4>
+				</div>
+			  </div>
+			</li>
+			<li class="span3">
+			  <div class="thumbnail">
+				<a href="product_details.html"><img src="themes/images/products/8.jpg" alt=""/></a>
+				<div class="caption">
+				  <h5>Clothes</h5>
+				  <p> 
+					ethnic clothes
+				  </p>
+				   <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&euro;222.00</a></h4>
+				</div>
+			  </div>
+			</li>
+		  </ul>
+
+
+	<hr class="soft"/>
+	</div>
+</div>
+<a href="compair.html" class="btn btn-large pull-right">Compare Product</a>
+	<div class="pagination">
+		<ul>
+		<li><a href="#">&lsaquo;</a></li>
+		<li><a href="#">1</a></li>
+		<li><a href="#">2</a></li>
+		<li><a href="#">3</a></li>
+		<li><a href="#">4</a></li>
+		<li><a href="#">...</a></li>
+		<li><a href="#">&rsaquo;</a></li>
+		</ul>
+	</div>
+<br class="clr"/>
+</div>
+
+@endsection
